f834f11b82579a8270d53f2b0c847b16a0ab4508
Ryan Ansberry <ansman92@gmail.com>
2019-05-03 18:45:06 -0700

    Did some initial event listeners, Will continue to add more and tweek them

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+const imgSelect = document.querySelectorAll('img');
+imgSelect.forEach((img) => {
+    img.addEventListener('mouseenter', (e) => {
+        e.target.style.transition = 'transform .2s';
+        e.target.style.transform = 'scale(1.05)'
+        e.target.style.zIndex = '-1';
+    });
+    img.addEventListener('mouseleave', (e) => {
+        e.target.style.transition = 'transform .2s';
+        e.target.style.transform = 'scale(1)';
+        e.target.style.zIndex = '-1';
+    });
+});
+
+document.addEventListener('auxclick', (e) => {
+    e.target.style.border = '2px solid lightblue';
+});
+
+document.addEventListener('dblclick', (e) => {
+    e.target.style.border = 'none';
+});
+
+const headers = document.querySelectorAll('h2');
+headers.forEach((h2) => {
+    h2.addEventListener('click', (e) => {
+        e.target.style.color = '#ffd800';
+    });
+});
+
+document.addEventListener('keydown', (e) => {
+});
